eb7d4ff2db3b76e6b4825698a827948999b33492
kd993595 <kepedu17@gmail.com>
2022-06-13 13:46:52 -0400

    update to https

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 let fileContent;
-const url = 'http://66.228.40.119/egg';
+const url = 'https://66.228.40.119/egg';
 var myChart;
 
 document.getElementById('datainputbtn').addEventListener('change', function (e) {
--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const url = 'http://66.228.40.119/user';
+const url = 'https://66.228.40.119/user';
 function validate() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
